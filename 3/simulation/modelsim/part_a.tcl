7a39433bed500fb9f1645f3ae345309ea0cc7dee
Eric <eriliu@ucdavis.edu>
2023-05-11 22:20:51 -0700

    Ignore Quartus files that aren't useful

--- 3/simulation/modelsim/part_a.tcl
+++
@@ -1,8 +0,0 @@
-vsim -gui -l msim_transcript {work.part_a } -L altera_ver -L fiftyfivenm_ver
-radix -hexadecimal
-add wave -position insertpoint  \
-{sim:/part_a /HEX0}
-add wave -position insertpoint  \
-{sim:/part_a /HEX1}
-add wave -position insertpoint -binary  \
-{sim:/part_a /SW}
--- 3/simulation/modelsim/part_a_run.tcl
+++
@@ -1,6 +0,0 @@
-for {set i 0} {$i < 16} {incr i} {
-    force -drive {sim:/part_a /SW} 10#$i 0
-    run 20 ns
-    # Wait for input so I can show the TA before proceeding
-    gets stdin
-}
